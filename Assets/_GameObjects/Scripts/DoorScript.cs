4d24c7ffee2174af6a987f06276693acaa042cb2
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-11-27 13:34:07 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/DoorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+    float speed = 0.05f;
+    float localXLimit = -5;
+	public void StartOpenDoor() {
+        StartCoroutine("OpenDoor");
+    }
+
+    IEnumerator OpenDoor() {
+        while (transform.localPosition.x > localXLimit){
+            yield return null;
+            transform.Translate(Vector3.left * speed);
+        }
+    }
+}
---
+++ Assets/_GameObjects/Scripts/EsferaScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsferaScript : MonoBehaviour {
+    public DoorScript ds;
+    public void OpenDoor() {
+        ds.StartOpenDoor();
+    }
+}
