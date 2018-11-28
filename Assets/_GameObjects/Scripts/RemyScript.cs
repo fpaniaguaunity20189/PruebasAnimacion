65940a3b94fa9f8d590a80aefa622c92b75438f7
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-11-28 12:17:04 +0100

    2018-11-28

---
+++ Assets/_GameObjects/Scripts/RemyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemyScript : MonoBehaviour {
+    private Animator miAnimator;
+
+	void Start () {
+        miAnimator = GetComponent<Animator>();
+	}
+
+	//Version bool
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            miAnimator.SetBool("EstaAndando", true);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            miAnimator.SetBool("EstaAndando", false);
+        }
+        if (Input.GetKeyDown(KeyCode.P)) {
+            miAnimator.SetTrigger("Punch");
+        }
+	}
+}
